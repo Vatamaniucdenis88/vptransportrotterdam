6fe2b9d69619b2ab57fb01d8699e0d19df2e1b77
Vatamaniucdenis88 <vatamaniuc88@gmail.com>
2020-05-10 17:37:03 +0100

    Updates

---
+++ precache-manifest.a63236463317aa4d4ebc7a32d42fc3dd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e065d9b0f831bcae82d44bb6d204e7a9",
+    "url": "/vptransportrotterdam/index.html"
+  },
+  {
+    "revision": "d9f0f520387d0dca1bfb",
+    "url": "/vptransportrotterdam/static/css/main.39bbd48f.chunk.css"
+  },
+  {
+    "revision": "927f765358131edcc735",
+    "url": "/vptransportrotterdam/static/js/2.4be5cef9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vptransportrotterdam/static/js/2.4be5cef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f0f520387d0dca1bfb",
+    "url": "/vptransportrotterdam/static/js/main.f298e801.chunk.js"
+  },
+  {
+    "revision": "28165cae3a557708c9e9",
+    "url": "/vptransportrotterdam/static/js/runtime-main.d3eda3d3.js"
+  },
+  {
+    "revision": "e93cadfa8638a9640470948ff05d55ff",
+    "url": "/vptransportrotterdam/static/media/about.e93cadfa.jpg"
+  },
+  {
+    "revision": "8e3e2b028cbb54e7870e6550cfe4db31",
+    "url": "/vptransportrotterdam/static/media/contact.8e3e2b02.jpg"
+  },
+  {
+    "revision": "ee56acc9462cb2c6acfa4d8bf6894e7a",
+    "url": "/vptransportrotterdam/static/media/cover.ee56acc9.jpg"
+  },
+  {
+    "revision": "162f1a94275c6e3b40a4dfd795e9ea21",
+    "url": "/vptransportrotterdam/static/media/logo.162f1a94.png"
+  },
+  {
+    "revision": "d879e1d1c22d02f00a87edfa3e09f9e2",
+    "url": "/vptransportrotterdam/static/media/service1.d879e1d1.png"
+  },
+  {
+    "revision": "08d0bbb6611c014b48eae4dd39095fda",
+    "url": "/vptransportrotterdam/static/media/service2.08d0bbb6.png"
+  },
+  {
+    "revision": "9838e669fa864df0781850a2870d3e18",
+    "url": "/vptransportrotterdam/static/media/service3.9838e669.png"
+  },
+  {
+    "revision": "f892f54bb3ac514d27395a75f815c596",
+    "url": "/vptransportrotterdam/static/media/service4.f892f54b.png"
+  },
+  {
+    "revision": "62d00bac7aab706d4c95a117e49eed08",
+    "url": "/vptransportrotterdam/static/media/service5.62d00bac.png"
+  }
+]);
